f1bcf1f84b072914ec75718a042edb1695976d9c
Kadyr <Kadyr@10.0.0.86>
2017-04-17 18:14:09 +0700

    First successful parsing of html file
    Placed the input file to the project base directory and successfully
    parsed html file

--- dev/parser/JSoup based/src/colomns_tokenization/ColomnTokenizer.java
+++ dev/parser/JSoup based/src/colomns_tokenization/ColomnTokenizer.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -10,12 +11,72 @@ import org.w3c.dom.css.CSSStyleSheet;
 import com.steadystate.css.parser.CSSOMParser;
 import com.steadystate.css.parser.SACParserCSS3;
 
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+
+
+
 public class ColomnTokenizer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//css_test(); [OK]
+			
+		try {
+			File input = new File("1.html");
+			Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+			Element page = doc.select("div.Sect1").first();
+			System.out.println(page.id());
+			
+			
+			} 
+		catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("In catch block !!!");
+			}
+			
+
+		
+	}
+	 public static void create_dir() {
+	      String dirname = "/tmp/user/java/bin";
+	      File d = new File(dirname);
+	      
+	      // Create directory now.
+	      d.mkdirs();
+	   }
+	 public static void show_dir() {
+	      File file = null;
+	      String[] paths;
+	  
+	      try {      
+	         // create new file object
+	         file = new File("/tmp");
+
+	         // array of files and directory
+	         paths = file.list();
+
+	         // for each name in the path array
+	         for(String path:paths) {
+	            // prints filename and directory name
+	            System.out.println(path);
+	         }
+	      }catch(Exception e) {
+	         // if any error occurs
+	         e.printStackTrace();
+	      }
+	   }
+	 
+	public static void css_test() {
+		// TODO Auto-generated method stub
+		
 
 		try {
+			
 			InputSource source = new InputSource( new StringReader ("h1 {background: #ffcc44}"));
 			CSSOMParser parser = new CSSOMParser(new SACParserCSS3());
 			CSSStyleSheet sheet;
@@ -41,7 +102,7 @@ public class ColomnTokenizer {
 		catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				System.out.println("Hello SAC!111");
+				System.out.println("In catch block !!!");
 			}
 			
 

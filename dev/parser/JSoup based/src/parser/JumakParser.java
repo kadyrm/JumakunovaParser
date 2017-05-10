5f81fb82171362ba84faf5bc5d46a84ec587be3c
lab112-pc09 <lab112-pc09@manas.com>
2017-05-10 19:19:28 -0400

    New JumakParser class for performing the main parsing task
    The parser accepts only the certain input, form of html document, being
    the output of the following conversion chain:
    pdf--->AdobeAcrobatXIpro--->doc--->LibreOffice--->odt--->
    LibreOffice-->xhtml. All input data is available at
    https://github.com/kadyrm/JumakunovaParser . The output of the parser is
    an html document with the markup reflecting the lexicographic structure
    of the Turkish-Kyrgyz dictionary by Gulzura Jumakunova

---
+++ dev/parser/JSoup based/src/parser/JumakParser.java
@@ -0,0 +1,19 @@
+package parser;
+
+public class JumakParser {
+/* <synopsis>
+ * The parser accepts only the certain input, form of html document, 
+ * being the output of the following conversion chain: 
+ * pdf--->AdobeAcrobatXIpro--->doc--->LibreOffice--->odt---> LibreOffice-->xhtml. 
+ * All input data is available at https://github.com/kadyrm/JumakunovaParser . 
+ * The output of the parser is an html document with the markup 
+ * reflecting the lexicographic structure of the Turkish-Kyrgyz dictionary 
+ * by Gulzura Jumakunova 
+ * </synopsis>
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
